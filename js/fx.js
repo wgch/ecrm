e13deb792c277def83f1491085942747410b255a
Wil <wilsongichuki@outlook.com>
2017-10-04 18:06:17 +0300

    updated sales table

--- js/fx.js
+++ js/fx.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
-    var table = $('#table-payg').DataTable( {
+    var tablePAYG = $('#table-payg').DataTable( {
         //scrollY:        "300px",
         scrollX:        true,
         scrollCollapse: true,
         paging:         false,
         fixedColumns:   {
-            leftColumns: 3,
+            leftColumns: 3
         },
         searching: false,
         ordering: false,
         info: false
-
-    } );
+    });
+    var tableSales = $('#table-sales').DataTable( {
+        //scrollY:        "300px",
+        responsive: true,
+        scrollX:        true,
+        scrollCollapse: true,
+        paging:         false,
+        fixedColumns:   {
+            leftColumns: 2,
+            rightColumns: 2
+        },
+        searching: false,
+        ordering: false,
+        info: false
+    });
 } );
